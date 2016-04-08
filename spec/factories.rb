dab526e6644b23a97126e61f1e85c01e080b80b9
Robert Sapunarich <robertsapunarich@gmail.com>
2016-04-08 11:19:14 -0700

    Include `FactoryGirl` and replace dummy data in tests.

---
+++ spec/factories.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :user do
+    sequence :email do |n|
+      "dummyEmail#{n}@example.com"
+    end
+    password "secretPassword"
+    password_confirmation "secretPassword"
+  end
+  
+end
