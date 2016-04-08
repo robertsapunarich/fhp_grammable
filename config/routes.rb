7d1d81ba1924379a73cf2c4c549a7f3c76d6a841
Robert Sapunarich <robertsapunarich@gmail.com>
2016-04-08 12:49:55 -0700

    Implement edit and update actions for grams. Write tests for successful and unsuccessful http requests.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root "grams#index"
-  resources :grams, only: [:new, :create, :show]
+  resources :grams, only: [:new, :create, :show, :edit, :update]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
